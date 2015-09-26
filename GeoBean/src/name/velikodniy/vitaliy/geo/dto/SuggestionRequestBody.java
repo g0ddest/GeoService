583354d26404ca36b86e93dc5105aa4a210d0ce1
Vitaliy Velikodniy <vitaliy@velikodniy.name>
2015-09-26 23:46:24 +0300

    add suggestions

---
+++ GeoBean/src/name/velikodniy/vitaliy/geo/dto/SuggestionRequestBody.java
@@ -0,0 +1,29 @@
+package name.velikodniy.vitaliy.geo.dto;
+
+public class SuggestionRequestBody{
+
+    private String query;
+    private int count;
+
+    public SuggestionRequestBody(String query, int count) {
+        this.query = query;
+        this.count = count;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+}
--- GeoBean/src/name/velikodniy/vitaliy/geo/provider/SuggestionProvider.java
+++ GeoBean/src/name/velikodniy/vitaliy/geo/provider/SuggestionProvider.java
@@ -1,11 +1,14 @@
 package name.velikodniy.vitaliy.geo.provider;
 
+import name.velikodniy.vitaliy.geo.dto.SuggestionRequestBody;
 import org.json.simple.JSONObject;
+import retrofit.Callback;
 
 import java.io.IOException;
 
 public interface SuggestionProvider {
 
-    JSONObject getSuggestions(String query) throws IOException;
+    JSONObject getSuggestions(SuggestionRequestBody query) throws IOException;
+    void getSuggestionsAsync(SuggestionRequestBody query, Callback<JSONObject> callback) throws IOException;
 
 }
